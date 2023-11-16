416ae12bca28b957b82bbc85692d19e1e08de933
m265394 <147636586+m265394@users.noreply.github.com>
2023-11-16 10:30:02 -0500

    Finished adding sprites, started on player 2 class

---
+++ game_parameters.py
@@ -0,0 +1,13 @@
+
+# Screen Dimensions (in pixels)
+SCREEN_WIDTH = 1680
+SCREEN_HEIGHT = 1050
+TILE_SIZE = 64
+
+# speed in pixels per frame
+MIN_SPEED = 0.5
+MAX_SPEED = 3.0
+PLAYER_SPEED = 3.0
+
+# number of lives
+NUM_LIVES = 3
---
+++ player_2.py
@@ -0,0 +1,58 @@
+
+import pygame
+from game_parameters import *
+
+class Player1(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image_idle = pygame.image.load("../assets/sprites/player_idle.png").convert()
+        self.image_idle.set_colorkey((0, 0, 0))
+        self.image_forward = pygame.image.load("../assets/sprites/player_2_walk.png").convert()
+        self.image_forward.set_colorkey((0, 0, 0))
+        self.image_reverse = pygame.transform.flip(self.image_forward, True, False)
+        self.image_reverse.set_colorkey((0, 0, 0))
+        self.image = self.image_forward
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.center = (x, y)
+        self.x_speed = 0
+        self.y_speed = 0
+
+    def move_up(self):
+        self.y_speed = PLAYER_SPEED
+
+    def move_down(self):
+        self.y_speed = -1 * PLAYER_SPEED
+
+    def move_left(self):
+        self.x_speed = -1 * PLAYER_SPEED
+        self.image = self.image_reverse
+
+    def move_right(self):
+        self.x_speed = PLAYER_SPEED
+        self.image = self.image_forward
+
+    def stop(self):
+        self.x_speed = 0
+        self.y_speed = 0
+
+    def update(self):
+        # TODO: need to check if player fish left the screen
+        self.x += self.x_speed
+        self.y -= self.y_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        if self.x >= (SCREEN_WIDTH - TILE_SIZE):
+            self.x_speed = 0
+        if self.x <= 0:
+            self.x_speed = 0
+
+        if self.y >= (SCREEN_HEIGHT - TILE_SIZE):
+            self.y_speed = 0
+        if self.y <= 0:
+            self.y_speed = 0
+
+    def draw(self, surf):
+        surf.blit(self.image, self.rect)
